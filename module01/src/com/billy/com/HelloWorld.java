bb3aea2476a042fab3bac0b4365c2d14817957f4
billy <418881713@qq.com>
2019-01-13 22:00:03 +0800

    12313

---
+++ module01/src/com/billy/com/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.billy.com;
+
+/**
+ * @author liubing(billy)
+ * @create 2019-01-13 21:48
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld!!!!");
+    }
+}
